0b0338456a805be41e03fafc605debea4e2dd441
Meghan Logan <meglo1029@gmail.com>
2017-09-29 04:02:22 -0700

    prettify

---
+++ JS/utility.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top //no need of parseInt here
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+ $('.type-it').typeIt({
+    strings: ['In a sea of millions of websites,stand out from the crowd.']
+  });
